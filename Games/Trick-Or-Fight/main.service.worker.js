dfda73563f7a02746685ac93397ca7f4191f2927
TheAmericanEmu <harringtondingc@gmail.com>
2025-12-09 20:20:02 -0600

    fixxed

--- Games/Trick-Or-Fight/main.service.worker.js
+++ Games/Trick-Or-Fight/main.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1765332532|891322000';
+const CACHE_VERSION = '1765333140|156230763';
 /** @type {string} */
 const CACHE_PREFIX = 'New Game Project-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
